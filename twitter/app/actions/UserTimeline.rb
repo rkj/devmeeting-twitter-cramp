34d9f03a40eeaa456c8c1857d25df3635ad0b031
Roman Kamyk <roman.kamyk@gmail.com>
2011-09-03 18:09:22 +0200

    simple client

--- twitter/app/actions/UserTimeline.rb
+++ twitter/app/actions/UserTimeline.rb
@@ -2,17 +2,17 @@ class UserTimeline < Cramp::Action
   use_fiber_pool# :size => 1000
 
   def start
-    DB.new.db_for_user(params[:screen_name]) do |db, id, pool, fiber|
+    DB.new.db_for_user(params[:screen_name]) do |db, id, conns|
       if db == nil
         render "WON!"
         finish
-        pool.release(fiber)
+        conns.each { |c| c.close }
       else
         #puts "Query on #{fiber.inspect}"
         q = db.aquery("SELECT * FROM statuses WHERE user_id = #{id} LIMIT 20")
         q.errback do |r|
           puts "Error123: #{r}"
-          pool.release(fiber)
+          conns.each { |c| c.close }
           finish
         end
         q.callback do |r|
@@ -23,7 +23,7 @@ class UserTimeline < Cramp::Action
           render result
           render ']'
           #puts "Releasing #{fiber.inspect} from #{db.inspect}"
-          pool.release(fiber)
+          conns.each { |c| c.close }
           finish
         end
       end
