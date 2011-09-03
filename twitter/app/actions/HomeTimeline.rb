50796cd3f89530452aea7adc733444d2c57299df
Janusz Bossy <YANOUSHek@Bare-Foot.local>
2011-09-03 11:17:18 +0200

    HomeTimeline

--- twitter/app/actions/HomeTimeline.rb
+++ twitter/app/actions/HomeTimeline.rb
@@ -1,8 +1,11 @@
+require 'em-synchrony/mysql2'
+require 'cramp/exception_handler'
 
 class HomeTimeline
 	def start
-
 		render "HomeTimeline"
+		name = params[:screen_name]
+		db = Mysql2::EM::Client.new(:host => "10.1.1.10", :username => "devcamp", :password => "devcamp", :database => "twitter1")
 		finish
 	end
 end
--- twitter/app/actions/Update.rb
+++ twitter/app/actions/Update.rb
@@ -1,5 +1,9 @@
+require 'em-synchrony/mysql2'
+require 'cramp/exception_handler'
+
 class Update < Cramp::Action
 	def start
+		db = Mysql2::EM::Client.new(:host => "10.1.1.10", :username => "devcamp", :password => "devcamp", :database => "twitter1")
 		render "Hello World!\n"
 		finish
 	end
