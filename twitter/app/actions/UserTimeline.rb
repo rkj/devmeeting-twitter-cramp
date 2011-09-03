e4a7c1197485bd2e91a66e436ab73a00cbc8c9f2
Janusz Bossy <YANOUSHek@Bare-Foot.local>
2011-09-03 12:10:46 +0200

    x

--- twitter/app/actions/UserTimeline.rb
+++ twitter/app/actions/UserTimeline.rb
@@ -1,6 +1,19 @@
 class UserTimeline < Cramp::Action
 	def start
-		render "UserTimeline"
-		finish
+		DB.new.db_for_user(params[:screen_name]) do |db, id|
+			if db == nil
+				render "WON!"
+				finish
+			end
+			db.aquery("SELECT * FROM statuses WHERE user_id = #{id}").callback do |r|
+				render '['
+				result = r.map do |row|
+					%{{"created_at":#{row['created_at']},"text":#{row['text']},"id":#{row['id']}}}
+				end.join(",\n")
+				render result
+				render ']'
+				finish
+			end
+		end
 	end
 end
--- twitter/app/model.rb
+++ twitter/app/model.rb
@@ -1,5 +1,36 @@
+require 'em-synchrony/mysql2'
+require 'cramp/exception_handler'
+
+
 class DB
+	@@db = EventMachine::Synchrony::ConnectionPool.new(size: 10) do
+		(1..4).map do |i|
+		Mysql2::EM::Client.new(:host => "10.1.1.10", :username => "devcamp", :password => "devcamp", :database => "twitter#{i}")
+		end
+	end
+
+	def db_for_user(name)
+		counter = 4
+		@@db.execute(true) do |acquired|
+			acquired.each do |db|
+				db.aquery("SELECT id FROM users WHERE screen_name = '#{name}'").callback do |r|
+					counter -= 1
+					if r.size > 0
+						r.each do |userRow|
+							counter = -1
+							yield db, userRow['id']
+						end
+					end
+					if counter == 0
+						yield nil, nil
+					end
+				end
+			end
+		end
+	end
+
   def test
     puts "TEST"
   end
+
 end
