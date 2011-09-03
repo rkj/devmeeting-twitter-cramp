63eaee1063be2f6b2d79ea9733fba68b8a9effcf
Janusz Bossy <YANOUSHek@Bare-Foot.local>
2011-09-03 13:13:16 +0200

    Post

--- twitter/app/actions/Update.rb
+++ twitter/app/actions/Update.rb
@@ -3,9 +3,23 @@ require 'cramp/exception_handler'
 
 class Update < Cramp::Action
 	def start
-		render "Hello World!\n"
-		p params
-		finish
+		name = params[:screen_name]
+		status = params[:status]
+		createdAt = Time.now.to_s
+
+		DB.new.db_for_user(name) do |db, id, pool, fiber|
+			db.aquery("INSERT INTO statuses (text, created_at, user_id) VALUES ('#{status}', '#{createdAt}', #{id}); SELECT LAST_INSERT_ID() AS x;")
+			query.callback do |r|
+				p r
+				pool.release(fiber)
+				finish
+			end
+			query.errback do |r| 
+				p r
+				finish
+			end
+		end
+
 	end
 end
 
