6b41b961d775b0e0f22c89f535834ebf754c7ac5
Janusz Bossy <YANOUSHek@Bare-Foot.local>
2011-09-03 15:41:51 +0200

    Done!

--- twitter/app/actions/UserTimeline.rb
+++ twitter/app/actions/UserTimeline.rb
@@ -12,7 +12,7 @@ class UserTimeline < Cramp::Action
         db.aquery("SELECT * FROM statuses WHERE user_id = #{id}").callback do |r|
           render '['
           result = r.map do |row|
-            %{{"created_at":#{row['created_at']},"text":#{row['text']},"id":#{row['id']}}}
+            %{{"created_at":"#{row['created_at']}","text":"#{row['text']}","id":#{row['id']}}}
           end.join(",\n")
           render result
           render ']'
